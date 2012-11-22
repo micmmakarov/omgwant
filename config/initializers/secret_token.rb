8bb7ba4699d13fab34cc54166977f1c6468f7968
Michael Makarov <info1117@gmail.com>
2012-11-22 13:44:26 -0800

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omgwant::Application.config.secret_token = 'dc85658c3cb1e822c402854902ae4f96caf7144c69799d42e34f6b041ca6560e0de9d9cad60a079b9dbbde95c3436f8145c1d693b697124a6084f98e406c48db'
