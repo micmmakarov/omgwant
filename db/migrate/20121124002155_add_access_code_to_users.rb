5d01b0c41aad97e5c57a9500cba6b4f92f0a3e8d
Michael Makarov <info1117@gmail.com>
2012-11-23 21:20:20 -0800

    long integers

--- db/migrate/20121124002155_add_access_code_to_users.rb
+++ db/migrate/20121124002155_add_access_code_to_users.rb
@@ -1,6 +1,6 @@
 class AddAccessCodeToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :facebook, :integer
+    add_column :users, :facebook, :integer, :limit => 8
     add_column :users, :facebook_token, :string
   end
 end
