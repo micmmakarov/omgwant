dbf81b7d2fc396c729ef50efb7f53f175509a81e
Michael Makarov <info1117@gmail.com>
2012-11-28 00:26:11 -0800

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name
 
-  #has_many :images
+  has_many :images
   has_many :cutes
 
   def liked_images
