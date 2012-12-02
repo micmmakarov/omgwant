fa3d9ae5705cd875150ab694fe4245ba6c246a00
Michael Makarov <info1117@gmail.com>
2012-12-02 00:24:11 -0800

    api decouple

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,6 +4,15 @@ class Api::UsersController < ApplicationController
     @user = User.find(params[:id])
     images = user.liked_images
     #render json: {image_collection: images.to_json(:methods => api_methods), user_info: user}
-    render json: @user.to_json(:methods => :liked_images)
+    render json: @user
   end
+
+  def user_likes
+    Image.current_user = current_user if user_signed_in?
+    @user = User.find(params[:id])
+    images = user.liked_images
+    #render json: {image_collection: images.to_json(:methods => api_methods), user_info: user}
+    render json: images
+  end
+
 end
