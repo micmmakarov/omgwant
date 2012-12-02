6524987b24315e6393b106e0acaff187c5ee6708
Michael Makarov <info1117@gmail.com>
2012-12-01 23:04:44 -0800

    api change

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,6 @@ class Api::UsersController < ApplicationController
     Image.current_user = current_user if user_signed_in?
     user = User.find(params[:id])
     images = user.liked_images
-    render json: images.to_json(:methods => [:title, :low_url, :url, :likes, :user_name, :computed_title, :like_action])
+    render json: [images.to_json(:methods => api_methods), user]
   end
 end
