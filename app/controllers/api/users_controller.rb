0090c809b0691f5d5e637344de9826a8c71e3369
Michael Makarov <info1117@gmail.com>
2012-12-01 23:48:24 -0800

    Better user profile api data

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::UsersController < ApplicationController
   def show
     Image.current_user = current_user if user_signed_in?
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     images = user.liked_images
-    render json: {image_collection: images.to_json(:methods => api_methods), user_info: user}
+    #render json: {image_collection: images.to_json(:methods => api_methods), user_info: user}
+    render json: @user.to_json(:methods => :liked_images)
   end
 end
